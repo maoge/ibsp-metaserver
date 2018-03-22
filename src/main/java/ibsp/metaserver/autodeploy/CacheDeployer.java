6112b459385618e21af4ad02fdcc59b5f144c90b
funow <445784427@qq.com>
2018-03-22 16:39:54 +0800

    MQ and cache delete service

--- src/main/java/ibsp/metaserver/autodeploy/CacheDeployer.java
+++ src/main/java/ibsp/metaserver/autodeploy/CacheDeployer.java
@@ -1,6 +1,12 @@
 package ibsp.metaserver.autodeploy;
 
+import java.util.Set;
+
 import ibsp.metaserver.bean.ResultBean;
+import ibsp.metaserver.dbservice.MetaDataService;
+import ibsp.metaserver.global.MetaData;
+import ibsp.metaserver.utils.CONSTS;
+import ibsp.metaserver.utils.Topology;
 
 public class CacheDeployer implements Deployer {
 
@@ -33,8 +39,41 @@ public class CacheDeployer implements Deployer {
 	@Override
 	public boolean deleteService(String serviceID, String sessionKey,
 			ResultBean result) {
-		// TODO Auto-generated method stub
-		return false;
+		Topology topo = MetaData.get().getTopo();
+		if (topo == null) {
+			result.setRetCode(CONSTS.REVOKE_NOK);
+			result.setRetInfo("MetaData topo is null!");
+			return false;
+		}
+		
+		// delete t_instance,t_instance_attr,t_topology
+		Set<String> sub = topo.get(serviceID, CONSTS.TOPO_TYPE_CONTAIN);
+		if (sub != null) {
+			for (String subID : sub) {
+				Set<String> subsub = topo.get(subID, CONSTS.TOPO_TYPE_CONTAIN);
+				if (subsub != null) {
+					for (String subsubID : subsub) {
+						if (!MetaDataService.deleteInstance(subID, subsubID, result))
+							return false;
+					}
+				}
+				
+				if (!MetaDataService.deleteInstance(serviceID, subID, result))
+					return false;
+			}
+		}
+		
+		// delete t_instance INST_ID = serviceID
+		if (!MetaDataService.deleteInstance(serviceID, serviceID, result))
+			return false;
+		
+		// delete t_service
+		if (!MetaDataService.deleteService(serviceID, result))
+			return false;
+		
+		// TODO MetaData clear using event:serviceMap,instanceDtlMap,topo
+		
+		return true;
 	}
 
 	@Override
--- src/main/java/ibsp/metaserver/autodeploy/MQDeployer.java
+++ src/main/java/ibsp/metaserver/autodeploy/MQDeployer.java
@@ -22,10 +22,12 @@ import ibsp.metaserver.bean.InstanceDtlBean;
 import ibsp.metaserver.bean.ResultBean;
 import ibsp.metaserver.dbservice.ConfigDataService;
 import ibsp.metaserver.dbservice.MQService;
+import ibsp.metaserver.dbservice.MetaDataService;
 import ibsp.metaserver.global.MetaData;
 import ibsp.metaserver.utils.CONSTS;
 import ibsp.metaserver.utils.ErlUtils;
 import ibsp.metaserver.utils.HttpUtils;
+import ibsp.metaserver.utils.Topology;
 
 public class MQDeployer implements Deployer {
 	
@@ -76,8 +78,41 @@ public class MQDeployer implements Deployer {
 	@Override
 	public boolean deleteService(String serviceID, String sessionKey,
 			ResultBean result) {
-		// TODO Auto-generated method stub
-		return false;
+		Topology topo = MetaData.get().getTopo();
+		if (topo == null) {
+			result.setRetCode(CONSTS.REVOKE_NOK);
+			result.setRetInfo("MetaData topo is null!");
+			return false;
+		}
+		
+		// delete t_instance,t_instance_attr,t_topology
+		Set<String> sub = topo.get(serviceID, CONSTS.TOPO_TYPE_CONTAIN);
+		if (sub != null) {
+			for (String subID : sub) {
+				Set<String> subsub = topo.get(subID, CONSTS.TOPO_TYPE_CONTAIN);
+				if (subsub != null) {
+					for (String subsubID : subsub) {
+						if (!MetaDataService.deleteInstance(subID, subsubID, result))
+							return false;
+					}
+				}
+				
+				if (!MetaDataService.deleteInstance(serviceID, subID, result))
+					return false;
+			}
+		}
+		
+		// delete t_instance INST_ID = serviceID
+		if (!MetaDataService.deleteInstance(serviceID, serviceID, result))
+			return false;
+		
+		// delete t_service
+		if (!MetaDataService.deleteService(serviceID, result))
+			return false;
+		
+		// TODO MetaData clear using event:serviceMap,instanceDtlMap,topo
+		
+		return true;
 	}
 
 	@Override
